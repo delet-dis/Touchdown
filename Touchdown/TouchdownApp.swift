35e58542eacaba6ce9afb9fad1eb0b9ca8fa33a5
delet-dis <igor@efimov.ga>
2021-11-14 22:25:06 +0700

    ✅ project init

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Igor Efimov on 14.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
